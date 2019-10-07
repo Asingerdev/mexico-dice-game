8e03e2ab3d33b548b2fc1ab1efb7b85d5e8150e9
Andrew Singer <andrewsinger@Andrews-MacBook-Pro-3.local>
2019-10-07 15:06:52 -0700

    Created player and game objects

--- js/game.js
+++ js/game.js
@@ -1,1 +1,23 @@
 console.log("linked")
+
+const game = {
+    rounds: 0,
+    firstRoll(){
+        Math.floor(Math.random())
+    },
+    rollDice(){
+
+    }
+}
+
+class Player {
+    constructor(name, lives) {
+        this.name = name;
+        this.lives = lives;
+    }
+}
+
+const p = new Player("greg", 3);
+console.log(p.lives)
+
+
