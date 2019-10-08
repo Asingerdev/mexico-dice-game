6fde4f9c8439945eacfde63b0a14d0686d61d20b
Andrew Singer <andrewsinger@Andrews-MacBook-Pro-3.local>
2019-10-08 14:07:18 -0700

    Created game's scoring system and built function that accepts player and computer's scores as arguments and determines the winner of the round

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 console.log("linked")
 
+//Game Object
 const game = {
     round: 0,
     setUp(){
@@ -28,6 +29,7 @@ const game = {
     }
 }
 
+//Player class
 class Player {
     constructor(name, lives) {
         this.name = name;
@@ -36,12 +38,43 @@ class Player {
     }
 }
 
+//Player and computer objects
 const p = new Player("greg", 3);
 
 const comp = {
     score: 0
 }
 
+//Global JQuery DOM elements
 const $round = $('#round');
 const $turn = $('#turn');
 const $roll = $('#roll-button');
+
+//Scoring System
+const scoreArr = [21, 66, 55, 44, 33, 22, 11];
+
+const checkScore = function(pScore,compScore){
+    if(scoreArr.includes(pScore) && scoreArr.includes(compScore)){
+        const index1 = scoreArr.indexOf(pScore);
+        const index2 = scoreArr.indexOf(compScore);
+        if(index2 > index1){
+            alert('Player wins round!')
+        }else if(index1 > index2){
+            alert('Computer wins round!');
+        }else if(index1 === index2){
+            alert('Round is a tie!');
+        }
+    }else if(scoreArr.includes(pScore) && !scoreArr.includes(compScore)){
+        alert('Player wins round!');
+    }else if(scoreArr.includes(compScore) && !scoreArr.includes(pScore)){
+        alert('Computer wins round!');
+    }else if(!scoreArr.includes(pScore) && !scoreArr.includes(compScore)){
+        if(pScore > compScore){
+            alert('Player wins round!');
+        }else if(compScore > pScore){
+            alert('Computer wins round!');
+        }else if(pScore === compScore){
+            alert('Round is a tie!');
+        }
+    }
+}
